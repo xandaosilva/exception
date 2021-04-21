9224e7a63386c54c68ae358fa60b37761a57008f
Alexandre Rogerio <xandaosilva93@hotmail.com>
2021-04-21 17:27:28 -0300

    Add Entities and develop Reservation class

---
+++ Exception/Entities/Reservation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exception.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkin, DateTime checkout)
+        {
+            return;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Room " + RoomNumber + ", ");
+            sb.Append("Check-in: " + CheckIn.ToString("dd/MM/yyyy") + ", ");
+            sb.Append("Check-out: " + CheckOut.ToString("dd/MM/yyyy") + ", ");
+            sb.Append(Duration() + " nights");
+            return sb.ToString();
+        }
+    }
+}
